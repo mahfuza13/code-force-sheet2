7288a4ba3c360bd54cac2e66f0a5dfa2ea6035d6
Mahfuza Akther <mahfuzaakther2004@gmail.com>
2025-09-25 16:48:13 +0600

    add my code force sheet 2

---
+++ D (Fixed Password).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int x ;
+    cin >> x ;
+    while (x!=1999)
+    {
+        cout << "Wrong" << endl ;
+        cin >> x ;
+    }
+    cout << "Correct" << endl ;
+    return 0 ;
+}
